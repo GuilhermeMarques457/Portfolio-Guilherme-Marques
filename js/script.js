6be1edeb4821b6d9e6960f218a85824870689821
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2023-06-15 16:10:05 -0300

    add-new-carrousel

--- js/script.js
+++ js/script.js
@@ -1,41 +1,31 @@
 "use strict";
 
 //////////////////////////////
+// Navigation functionality //
+//////////////////////////////
+
+const navAbout = document.querySelector(".btn-navigation-about");
+const navProjects = document.querySelector(".btn-navigation-projects");
+const aboutSection = document.querySelector(".about-me-section");
+const projectsSection = document.querySelector(".projects-section");
+
+navAbout.addEventListener("click", function () {
+  navAbout.classList.add("navigation-active");
+  navProjects.classList.remove("navigation-active");
+  aboutSection.classList.remove("element-hidden");
+  projectsSection.classList.add("element-hidden");
+});
+
+navProjects.addEventListener("click", function () {
+  navAbout.classList.remove("navigation-active");
+  navProjects.classList.add("navigation-active");
+  aboutSection.classList.add("element-hidden");
+  projectsSection.classList.remove("element-hidden");
+});
+
+//////////////////////////////
 // Carrousell functionality //
 //////////////////////////////
-const boxSlides = document.querySelector(".box-slides");
-const slides = document.querySelectorAll(".box-slide");
-const btnRight = document.querySelector(".btn-right");
-const btnLeft = document.querySelector(".btn-left");
-
-let slideIndex = 0;
-
-function updateSlideClasses() {
-  slides.forEach((element) => {
-    removeAllClasses(element);
-  });
-
-  for (let index = 0; index < slides.length; index++) {
-    const slide = slides[index];
-
-    if (index === slideIndex) {
-      slide.classList.add("element-showed");
-
-      if (index === 0) {
-        addLeftClasses(slides[slides.length - 1]);
-        addRightClasses(slides[index + 1]);
-      } else if (index === slides.length - 1) {
-        addLeftClasses(slides[index - 1]);
-        addRightClasses(slides[0]);
-      } else {
-        addLeftClasses(slides[index - 1]);
-        addRightClasses(slides[index + 1]);
-      }
-
-      break;
-    }
-  }
-}
 
 function addRightClasses(slide) {
   slide.classList.add("element-right", "element-showed");
@@ -49,26 +39,127 @@ function removeAllClasses(slide) {
   slide.classList.remove("element-left", "element-showed", "element-right");
 }
 
-function nextSlide() {
-  slideIndex++;
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
+//End
+
+//////////////////////////////////
+// Carrousell functionality ABOUT //
+//////////////////////////////////
+
+const boxSlidesAbout = document.querySelector(".box-slides-about");
+const slidesAbout = document.querySelectorAll(".box-slide-about");
+const btnRightAbout = document.querySelector(".btn-right-about");
+const btnLeftAbout = document.querySelector(".btn-left-about");
+
+let slideIndexAbout = 0;
+
+function updateSlideClassesAbout() {
+  slidesAbout.forEach((element) => {
+    removeAllClasses(element);
+  });
+
+  for (let index = 0; index < slidesAbout.length; index++) {
+    const slide = slidesAbout[index];
+
+    if (index === slideIndexAbout) {
+      slide.classList.add("element-showed");
+
+      if (index === 0) {
+        addLeftClasses(slidesAbout[slidesAbout.length - 1]);
+        addRightClasses(slidesAbout[index + 1]);
+      } else if (index === slidesAbout.length - 1) {
+        addLeftClasses(slidesAbout[index - 1]);
+        addRightClasses(slidesAbout[0]);
+      } else {
+        addLeftClasses(slidesAbout[index - 1]);
+        addRightClasses(slidesAbout[index + 1]);
+      }
+
+      break;
+    }
   }
-  updateSlideClasses();
 }
 
-function previousSlide() {
-  slideIndex--;
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
+function nextSlideAbout() {
+  slideIndexAbout++;
+  if (slideIndexAbout >= slidesAbout.length) {
+    slideIndexAbout = 0;
   }
-  updateSlideClasses();
+  updateSlideClassesAbout();
 }
 
-btnRight.addEventListener("click", nextSlide);
-btnLeft.addEventListener("click", previousSlide);
+function previousSlideAbout() {
+  slideIndexAbout--;
+  if (slideIndexAbout < 0) {
+    slideIndexAbout = slidesAbout.length - 1;
+  }
+  updateSlideClassesAbout();
+}
 
-updateSlideClasses();
+btnRightAbout.addEventListener("click", nextSlideAbout);
+btnLeftAbout.addEventListener("click", previousSlideAbout);
+
+updateSlideClassesAbout();
+
+//End
+
+//////////////////////////////////
+// Carrousell functionality ABOUT //
+//////////////////////////////////
+
+const boxSlidesProjects = document.querySelector(".box-slides-projects");
+const slidesProjects = document.querySelectorAll(".box-slide-projects");
+const btnRightProjects = document.querySelector(".btn-right-projects");
+const btnLeftProjects = document.querySelector(".btn-left-projects");
+
+let slideIndexProjects = 0;
+
+function updateSlideClassesProjects() {
+  slidesProjects.forEach((element) => {
+    removeAllClasses(element);
+  });
+
+  for (let index = 0; index < slidesProjects.length; index++) {
+    const slide = slidesProjects[index];
+
+    if (index === slideIndexProjects) {
+      slide.classList.add("element-showed");
+
+      if (index === 0) {
+        addLeftClasses(slidesProjects[slidesProjects.length - 1]);
+        addRightClasses(slidesProjects[index + 1]);
+      } else if (index === slidesProjects.length - 1) {
+        addLeftClasses(slidesProjects[index - 1]);
+        addRightClasses(slidesProjects[0]);
+      } else {
+        addLeftClasses(slidesProjects[index - 1]);
+        addRightClasses(slidesProjects[index + 1]);
+      }
+
+      break;
+    }
+  }
+}
+
+function nextSlideProjects() {
+  slideIndexProjects++;
+  if (slideIndexProjects >= slidesProjects.length) {
+    slideIndexProjects = 0;
+  }
+  updateSlideClassesProjects();
+}
+
+function previousSlideProjects() {
+  slideIndexProjects--;
+  if (slideIndexProjects < 0) {
+    slideIndexProjects = slidesProjects.length - 1;
+  }
+  updateSlideClassesProjects();
+}
+
+btnRightProjects.addEventListener("click", nextSlideProjects);
+btnLeftProjects.addEventListener("click", previousSlideProjects);
+
+updateSlideClassesProjects();
 
 //End
 
